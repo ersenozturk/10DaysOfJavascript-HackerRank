e805abba2303eeeaa93eb4c31c7e7d150bbc0c41
ersenozturk <70ozturkersen@gmail.com>
2022-03-24 20:17:57 +0300

    folder name chanced

--- Day-2/Arrays.js
+++
@@ -1,9 +0,0 @@
-// https://www.hackerrank.com/challenges/js10-arrays/problem?isFullScreen=true
-
-function getSecondLargest(nums) {
-    nums = new Set(nums);
-    nums = Array.from(nums);
-    nums = nums.sort((a,b)=>{return a-b})
-    return nums[nums.length-2]
-    
-}
