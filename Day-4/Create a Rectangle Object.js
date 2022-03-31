b3f38910f4b8c9276a1d991f8c57ee25641eee15
ersen <70ozturkersen@gmail.com>
2022-03-31 21:08:06 +0300

    Day-4  Create a Rectangle Object challange done

---
+++ Day-4/Create a Rectangle Object.js
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/js10-objects/problem?isFullScreen=true
+
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a*b
+    }
+}
